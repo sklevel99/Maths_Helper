1f2d1c2c85ca17535d82975b7cff0ae1df2059c5
Khooshal Seechurn <khooshalseechurn@gmail.com>
2022-11-07 18:30:04 -0500

    deleted a bunch of shit again..my bad

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "helpers", to: "helpers#index"
+  root to: "topics#index"
 
 end
